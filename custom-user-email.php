fa53dfa9d0214bc2361d72d4d6a3da967d10043f
bulini <pinobulini@gmail.com>
2017-11-14 00:59:53 +0100

    languages and updates

--- custom-user-email.php
+++ custom-user-email.php
@@ -1,97 +1,103 @@
 <?php
 /**
- * Plugin Name:     My User Email
- * Plugin URI:      https://www.giuseppesurace.com/
- * Description:     Quickly edit your new user welcome notification. No configuration needed, just change the subject the email text using all useful placeholedrs.
- * Author:          Giuseppe
- * Author URI:      https://www.giuseppesurace.com/
- * Text Domain:     custom-user-email
- * Domain Path:     /languages
- * Version:         0.1.0
- *
- * @package         Custom_User_Email
- */
+* Plugin Name:     New user custom email
+* Plugin URI:      https://www.giuseppesurace.com/
+* Description:     Quickly edit your new user welcome notification. No configuration needed, just change the subject the email text using all useful placeholedrs.
+* Author:          Giuseppe
+* Author URI:      https://www.giuseppesurace.com/
+* Text Domain:     custom-user-email
+* Domain Path:     /languages
+* Version:         0.1.0
+*
+* @package         Custom_User_Email
+*/
 
-define('PLUGIN_NAME','Custom New User Email');
+define('PLUGIN_NAME','New user custom email');
 
 require_once('options-page.php');
 
 add_action( 'plugins_loaded', 'custom_user_email_textdomain' );
 /**
- * Load plugin textdomain.
- *
- * @since 1.0.0
- */
+* Load plugin textdomain.
+*
+* @since 1.0.0
+*/
 function custom_user_email_textdomain() {
-  load_plugin_textdomain( 'custom_user_email', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
+  load_plugin_textdomain( 'custom-user-email', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 
- /**
- * redefine new user notification function
- *
- * emails new users their login info
- *
- * @author	Giuseppe Surace <info@giuseppesurace.com>
- * @param 	integer $user_id user id
- */
- if ( !function_exists( 'wp_new_user_notification' ) ) {
-     function wp_new_user_notification( $user_id ) {
+add_action('admin_head', 'wp_new_user_style');
 
-         // html content type
-         add_filter( 'wp_mail_content_type', 'wpmail_content_type' );
+function wp_new_user_style() {
+  echo '<style>.wp_new_user_panel { padding:5px; background:#fff; border-left:4px solid #2ada70; font-size:14px; box-shadow: 0 1px 1px 0 rgba(0,0,0,.1); }</style>';
+}
 
-         // new user object
-         $user_object = new WP_User( $user_id );
-         $user_email = stripslashes( $user_object->user_email );
-         $site_url = get_option( 'siteurl');
+/**
+* redefine new user notification function
+*
+* emails new users their login info
+*
+* @author	Giuseppe Surace <info@giuseppesurace.com>
+* @param 	integer $user_id user id
+*/
+if ( !function_exists( 'wp_new_user_notification' ) ) {
+  function wp_new_user_notification( $user_id ) {
 
-         $_placeholders = array(
-            'first_name'=> $user_object->first_name,
-            'last_name' => $user_object->last_name,
-            'site_url'  => $site_url,
-            'username'  => $user_object->user_login
-        );
+    // html content type
+    add_filter( 'wp_mail_content_type', 'wpmail_content_type' );
 
-         //to do: customize....
-         $subject = get_option('_email_subject');
+    // new user object
+    $user_object = new WP_User( $user_id );
+    $user_email = stripslashes( $user_object->user_email );
+    $site_url = get_option( 'siteurl');
 
-         $subject = replaceTags($subject, $_placeholders);
-         
-         $headers = 'From: '.get_option('blogname').' <'.get_option('admin_email').'>';
+    $_placeholders = array(
+      'first_name'=> $user_object->first_name,
+      'last_name' => $user_object->last_name,
+      'site_url'  => $site_url,
+      'username'  => $user_object->user_login
+    );
 
-         // if admin email on (todo) ---
-         $message  = __("A new user has been created", "custom_user_email")."\r\n\r\n";
-         $message .= 'Email: '.$user_email."\r\n";
-         @wp_mail( get_option( 'admin_email' ), __('New user created', 'custom_user_email'), $message, $headers );
-         // end if admin email -----
+    //to do: customize....
+    $subject = get_option('_email_subject');
 
-         //load email template w wpautop
-         $new_user_message = wpautop(get_option('_email_text'));
-         //replace mai tags 
-         $new_user_message = replaceTags($new_user_message, $_placeholders);
+    $subject = replaceTags($subject, $_placeholders);
 
-         ob_start();
-         include plugin_dir_path( __FILE__ ).'/email/_welcome_new_user.php';
-         $message = ob_get_contents();
-         ob_end_clean();
+    $headers = 'From: '.get_option('blogname').' <'.get_option('admin_email').'>';
 
-         @wp_mail( $user_email, $subject, $message, $headers );
+    // if admin email on (todo) ---
+    $message  = __('A new user has been created', 'custom-user-email')."\r\n\r\n";
+    $message .= 'Email: '.$user_email."\r\n";
+    @wp_mail( get_option( 'admin_email' ), __('New user created', 'custom-user-email'), $message, $headers );
+    // end if admin email -----
 
-         // remove html content type
-         remove_filter ( 'wp_mail_content_type', 'wpmail_content_type' );
-     }
- }
+    //load email template w wpautop
+    $new_user_message = wpautop(get_option('_email_text'));
+    //replace mai tags
+    $new_user_message = replaceTags($new_user_message, $_placeholders);
+
+    ob_start();
+    include plugin_dir_path( __FILE__ ).'/email/_welcome_new_user.php';
+    $message = ob_get_contents();
+    ob_end_clean();
+
+    @wp_mail( $user_email, $subject, $message, $headers );
+
+    // remove html content type
+    remove_filter ( 'wp_mail_content_type', 'wpmail_content_type' );
+  }
+}
 
 
- /**
- * wpmail_content_type
- * allow html emails
- * @author Giuseppe Surace <info@giuseppesurace.com>
- * @return string
- */
+/**
+* wpmail_content_type
+* allow html emails
+* @author Giuseppe Surace <info@giuseppesurace.com>
+* @return string
+*/
 function wpmail_content_type() {
-    return 'text/html';
+  return 'text/html';
 }
 
 function replaceTags($template = "", $tags = array()) {
--- options-page.php
+++ options-page.php
@@ -1,55 +1,66 @@
 <?php
-// create custom plugin settings menu
+// create custom custom_user_email_menu
 add_action('admin_menu', 'custom_user_email_menu');
 
 function custom_user_email_menu() {
-
-	//create new top-level menu
+	//create new menu for plugin
 	add_menu_page('User email Settings', 'New User email', 'administrator', __FILE__, 'custom_user_email_settings_page' , plugins_url('/mail.png', __FILE__) );
-
 	//call register settings function
 	add_action( 'admin_init', 'custom_user_email_settings' );
 }
-
 
 function custom_user_email_settings() {
 	//register settings
 	register_setting( 'custom-user-email-settings-group', '_email_subject' );
 	register_setting( 'custom-user-email-settings-group', '_email_text' );
-	register_setting( 'custom-user-email-settings-group', '_email_footer_text' );
+	//register_setting( 'custom-user-email-settings-group', '_email_footer_text' );
 }
 
 function custom_user_email_settings_page() {
-?>
-<div class="wrap">
-<h1><?php echo PLUGIN_NAME; ?></h1>
+	?>
+	<div class="wrap">
+		<h1><?php echo PLUGIN_NAME; ?></h1>
 
-<span>[[site_url]] [[username]] [[first_name]] [[last_name]]</span>
+		<div class="wp_new_user_panel">
+			<b><?php _e('Placeholders', 'custom-user-email'); ?></b>
+			<p><?php _e('You can use these placeholder to customize each message. (Ex Dear [[first_name]]).', 'custom-user-email'); ?></p>
+			<b>[[site_url]] [[username]] [[first_name]] [[last_name]]</b>
+		</div>
 
-<form method="post" action="options.php">
-    <?php settings_fields( 'custom-user-email-settings-group' ); ?>
-    <?php do_settings_sections( 'custom-user-email-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row"><?php _e('Welcome email Subject', 'custom_user_email'); ?></th>
-        <td><input type="text" name="_email_subject" value="<?php echo esc_attr( get_option('_email_subject') ); ?>" /></td>
-        </tr>
-         
-        <tr valign="top">
-        <th scope="row"><?php _e('Welcome email text', 'custom_user_email'); ?></th>
-        <td><?php $content = get_option('_email_text');
-                  $editor_id = '_email_text';
-                  wp_editor( $content, $editor_id );?></td>
-        </tr>
-        
-        <tr valign="top">
-        <th scope="row"><?php _e('Welcome email Footer Text', 'custom_user_email'); ?></th>
-        <td><input type="text" name="_email_footer_text" value="<?php echo esc_attr( get_option('_email_footer_text') ); ?>" /></td>
-        </tr>
-    </table>
-    
-    <?php submit_button(); ?>
+		<form method="post" action="options.php">
+			<?php settings_fields( 'custom-user-email-settings-group' ); ?>
+			<?php do_settings_sections( 'custom-user-email-settings-group' ); ?>
+			<table class="form-table">
 
-</form>
-</div>
+				<tr valign="top">
+					<th scope="row">
+						<?php _e('Welcome email Subject', 'custom-user-email'); ?>
+					</th>
+					<td>
+						<input type="text" name="_email_subject" class="regular-text" value="<?php echo esc_attr( get_option('_email_subject') ); ?>" />
+						<small><?php _e('Your welcome email subject for new user notification', 'custom-user-email'); ?></small>
+					</td>
+				</tr>
+
+				<tr valign="top">
+					<th scope="row">
+						<?php _e('Welcome email text', 'custom-user-email'); ?>
+					</th>
+					<td>
+						<?php
+						$content = get_option('_email_text');
+						$editor_id = '_email_text';
+						wp_editor( $content, $editor_id ); ?>
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row"></th>
+					<td><?php submit_button(); ?></td>
+				</tr>
+
+			</table>
+		</form>
+
+	</div>
 <?php } ?>
